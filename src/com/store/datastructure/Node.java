3e343e2923c334a2966c1052af41231ecbaa9fc8
Erick Rocha <erickamil@gmail.com>
2026-06-12 21:54:48 -0300

    feat: add Node class for custom datastructure implementation

---
+++ src/com/store/datastructure/Node.java
@@ -0,0 +1,11 @@
+package com.store.datastructure;
+
+public class Node<T> {
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
