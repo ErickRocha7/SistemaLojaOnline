aebb2887b70bcc3a72c16f503773b149d2a4a450
Erick Rocha <erickamil@gmail.com>
2026-06-13 12:19:59 -0300

    Refactor: Add Deitel chapter comments to ProductNotFoundException

--- src/com/store/exception/ProductNotFoundException.java
+++ src/com/store/exception/ProductNotFoundException.java
@@ -1,7 +1,15 @@
 package com.store.exception;
 
+/**
+ * <h1>Capítulo 11 – Tratamento de exceção</h1>
+ * Exceção disparada quando um produto não é localizado pelo ID informado.
+ * A mensagem é construída utilizando concatenação de {@code String}
+ * (<b>Capítulo 3 – Strings</b>) e passada ao construtor pai com
+ * {@code super} (<b>Capítulo 9 – Herança</b>).
+ */
 public class ProductNotFoundException extends Exception {
     public ProductNotFoundException(String id) {
+        // Capítulo 3: concatenação de strings com operador '+'
         super("Produto com ID " + id + " não encontrado.");
     }
 }
