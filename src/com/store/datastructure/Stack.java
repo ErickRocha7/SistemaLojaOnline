310dc69f3993deb82324bca4cd6a7ba589a68e3a
Erick Rocha <erickamil@gmail.com>
2026-06-12 21:56:07 -0300

    feat: add Stack class for managing undo history

---
+++ src/com/store/datastructure/Stack.java
@@ -0,0 +1,45 @@
+package com.store.datastructure;
+
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private Node<T> top;
+    private int size;
+
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+
+    public void push(T item) {
+        Node<T> newNode = new Node<>(item);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return size;
+    }
+}
